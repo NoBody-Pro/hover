40e7c2a6fdf57a966a5d96d3d1200a64ddb0d566
NoBody <96974069+NoBody-Pro@users.noreply.github.com>
2022-03-27 20:26:59 +0530

    Add files via upload

---
+++ pro2.js
@@ -0,0 +1,12 @@
+var circule = document.getElementById("circule")
+function hover()
+{
+    circule.style.width = "250px"; 
+    circule.style.height = "250px";
+}
+function hoverend()
+{
+    circule.style.width = "50px"; 
+    circule.style.height = "50px";
+}
+
